8b45c2fe35d60f1d6d595a93040042a738960b8b
Krasimir Hristov <hkrasimir53@gmail.com>
2022-04-12 18:29:44 +0300

    Implemented highlights controller with videos

---
+++ SportNewsApp/Controllers/HighlightsController.cs
@@ -0,0 +1,17 @@
+﻿
+namespace SportNewsApp.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.IO;
+    using System.Linq;
+
+    public class HighlightsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var videos = Directory.GetFiles("wwwroot/videos");
+            videos = videos.Select(d => d.Replace("\\", "/")).Select(d => d.Replace("wwwroot", string.Empty)).ToArray();
+            return View(videos);
+        }
+    }
+}
